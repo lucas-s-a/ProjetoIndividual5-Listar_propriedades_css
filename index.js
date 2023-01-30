bea409e566a02ee36fb871f5d9cc7ccce3c88d7b
lucas-s-a <lucassalves485@outlook.com>
2023-01-30 06:01:09 -0300

    commit atualização index 2

--- index.js
+++ index.js
@@ -5,35 +5,35 @@ import fs from 'fs'
 
 ProgMain()
 
-function ProgMain(){
+function ProgMain() {
     inquirer
-    .prompt([
+        .prompt([
             {
-                type:'list',
-                name:'escolher',
-                message:'O que voce deseja fazer?',
-                choices:[
-                    'Criar Lista',
+                type: 'list',
+                name: 'escolher',
+                message: 'O que voce deseja fazer?',
+                choices: [
+                    'Criar Lista Nova',
                     'Editar Lista',
                     'Ver Dados',
                     'Sair'
                 ],
             },
-    ])
-    .then((anwer) => {
-        const escolher = anwer['escolher']
+        ])
+        .then((anwer) => {
+            const escolher = anwer['escolher']
 
-        if (escolher === 'Criar Lista'){
-            Listapropriedades()
-        }else if (escolher === 'Editar Lista'){
-            EditarPropriedades()
-        }else if(escolher === 'Ver Dados'){
-            Visualizardados()
-        }else if  (escolher === 'Sair'){
-            console.log(chalk.bgGreen.underline.blackBright('Obrigado Por nos escolher ,Tenha um bom dia'))
-            process.exit()
-        }
-    })
+            if (escolher === 'Criar Lista Nova') {
+                Listapropriedades()
+            } else if (escolher === 'Editar Lista') {
+                EditarPropriedades()
+            } else if (escolher === 'Ver Dados') {
+                Visualizardados()
+            } else if (escolher === 'Sair') {
+                console.log(chalk.bgGreen.blackBright('Obrigado Por nos escolher ,Tenha um bom dia'))
+                process.exit()
+            }
+        })
 }
 
 const command = prompt()
@@ -41,145 +41,160 @@ const command = prompt()
 const Listapropriedades = (entrada) => {
     let lista = []
     while (entrada != "sair") {
-        entrada = command("Insira uma propriedade CSS:")
-        if(lista.indexOf(`"${entrada}"`) == -1 && entrada != 'sair'){
+        entrada = command("Insira uma propriedade CSS ou 'sair' para Encerrar:").toLocaleLowerCase()
+        if (lista.indexOf(`"${entrada}"`) == -1 && entrada != 'sair') {
             lista.push(`"${entrada}"`);
-        }else if(entrada == 'sair' ){
-            console.log('Encerrando lista ...')
+        } else if (entrada == 'sair') {
+            console.log(chalk.yellow('Encerrando lista ...'))
         }
-        else if(lista.indexOf(`"${entrada}"`) != -1){
-            console.log('Regra já adicionada')
+        else if (lista.indexOf(`"${entrada}"`) != -1) {
+            console.log(chalk.yellowBright('Regra já adicionada na lista'))
         }
     }
-    let result= lista.sort().join("\n")
-    console.log(result)
-    CriarArquivo(lista)    
+    let result = lista.sort().join("\n")
+    console.log(chalk.bgCyanBright(result))
+    CriarArquivo(lista)
 }
 
-function CriarArquivo(lista){
-    console.log(lista)
+function CriarArquivo(lista) {
     inquirer.prompt([
         {
-            name:'lis',
-            message:'Deseja criar nova lista em arquivo?',
+            name: 'lis',
+            message: 'Deseja criar nova lista em arquivo?',
 
         },
     ])
-    .then((answer) => {
-        if(answer.lis == 'sim'){
-            console.info(answer['lis'])
-            
-            const listacriada = 'novalista'
+        .then((answer) => {
+            let ajuste = answer.lis.toLocaleLowerCase()
+            if (ajuste == 'sim') {
+                const listacriada = 'novalista'
 
-            if(!fs.existsSync('plista')){
-                fs.mkdirSync('plista')
-            }
-            if(fs.existsSync(`plista/${listacriada}.json`)){
-                console.log(chalk.bgCyan.black('Nova Lista Criada!'),)
-            }
-            fs.writeFileSync(`plista/${listacriada}.json`,`{"list":[${lista}]}`,
-            function (err){
-                console.log(err)
-            },)
-            console.log(chalk.green('Arquivo Criado!'))
-            ProgMain()
-        }else{
-            console.log('Encerrando a lista ...')
-        }
-    })
-}
-function EditarPropriedades(entrada){
-    inquirer
-    .prompt([
-        {
-            name:'listaatual',
-            message:'Nome do arquivo que possui os itens?'
-
-        },
-    ])
-    .then((answer) => {
-        const listaedit =answer['listaatual']
-        const listaed = Puxalista(listaedit)
-        
-        let listavelha = listaed.list
-        let lista = []
-        for (let cot=0; cot<listavelha.length;cot++){
-            lista.push(`"${listavelha[cot]}"`)
-        }
-        inquirer
-        .prompt([
-            {
-                type:'list',
-                name:'escolheritens',
-                message:'O que voce deseja fazer?',
-                choices:[
-                    'Adicionar na Lista',
-                    'Remover da Lista'
-                ],
-            },
-        ])
-        .then((anwer) => {
-            const escolheritens = anwer['escolheritens']
-
-            if (escolheritens === 'Adicionar na Lista'){
-                while (entrada != "sair") {
-                    entrada = command("Insira uma propriedade CSS:")
-                    if(lista.indexOf(`"${entrada}"`) == -1 && entrada != 'sair'){
-                        lista.push(`"${entrada}"`);
-                    }else if(entrada == 'sair' ){
-                        console.log('Encerrando lista ...')
-                    }else if(lista.indexOf(`"${entrada}"`) != -1){
-                        console.log('Regra já adicionada')
-                    }
+                if (!fs.existsSync('plista')) {
+                    fs.mkdirSync('plista')
                 }
-                let result= lista.sort().join("\n")
-                console.log(result)
-                CriarArquivo(lista)
-            }else if (escolheritens === 'Remover da Lista'){
-                while (entrada != "sair") {
-                    entrada = command("Insira uma propriedade CSS:")
-                    if(lista.indexOf(`"${entrada}"`) == -1 && entrada != 'sair'){
-                        console.log('Não existe esse elemento na lista')
-                    }else if(entrada == 'sair' ){
-                        console.log('Encerrando lista ...')
-                    }else if(lista.indexOf(`"${entrada}"`) != -1){
-                        lista.splice(lista.indexOf(`"${entrada}"`),1)
-                        console.log(`A propriedade ${entrada} foi removida da lista`)
-                    }
+                if (fs.existsSync(`plista/${listacriada}.json`)) {
+                    console.log(chalk.bgCyan.black('Nova Lista Criada!'),)
                 }
-                let result= lista.sort().join("\n")
-                console.log(result)
-                CriarArquivo(lista)
+                fs.writeFileSync(`plista/${listacriada}.json`, `{"list":[${lista}]}`,
+                    function (err) {
+                        console.log(err)
+                    },)
+                console.log(chalk.green('Arquivo Criado!'))
+                ProgMain()
+            } else if (ajuste == 'nao' || ajuste == 'não') {
+                console.log(chalk.bgYellow('Ok! Retornando a página inicial'))
+                ProgMain()
+            } else {
+                console.log(chalk.red('Comando inválido Retornando a página inicial!'))
+                ProgMain()
             }
         })
-    })
 }
-function Puxalista(listaedit){
-    const accountJSON = fs.readFileSync(`plista/${listaedit}.json`,{
-        encoding:'utf-8',
-        flag:'r',//quebra linha
+function EditarPropriedades(entrada) {
+    inquirer
+        .prompt([
+            {
+                name: 'listaatual',
+                message: 'Nome do arquivo que possui os itens?'
+
+            },
+        ])
+        //começa aqui
+        .then((answer) => {
+            try {
+                const listaedit = answer['listaatual']
+                const listaed = Puxalista(listaedit)
+
+                let listavelha = listaed.list
+                let lista = []
+                for (let cot = 0; cot < listavelha.length; cot++) {
+                    lista.push(`"${listavelha[cot]}"`)
+                }
+                inquirer
+                    .prompt([
+                        {
+                            type: 'list',
+                            name: 'escolheritens',
+                            message: 'O que voce deseja fazer?',
+                            choices: [
+                                'Adicionar na Lista',
+                                'Remover da Lista'
+                            ],
+                        },
+                    ])
+                    .then((anwer) => {
+                        const escolheritens = anwer['escolheritens']
+
+                        if (escolheritens === 'Adicionar na Lista') {
+                            while (entrada != "sair") {
+                                entrada = command("Insira uma propriedade CSS ou 'sair' para Encerrar:").toLocaleLowerCase()
+                                if (lista.indexOf(`"${entrada}"`) == -1 && entrada != 'sair') {
+                                    lista.push(`"${entrada}"`);
+                                } else if (entrada == 'sair') {
+                                    console.log(chalk.yellow('Finalizando ...'))
+                                } else if (lista.indexOf(`"${entrada}"`) != -1) {
+                                    console.log(chalk.bgYellowBright('Regra já adicionada'))
+                                }
+                            }
+                            let result = lista.sort().join("\n")
+                            console.log(chalk.bgCyanBright(result))
+                            CriarArquivo(lista)
+                        } else if (escolheritens === 'Remover da Lista') {
+                            while (entrada != "sair") {
+                                entrada = command("Insira uma propriedade CSS ou 'sair' para Encerrar:").toLocaleLowerCase()
+                                if (lista.indexOf(`"${entrada}"`) == -1 && entrada != 'sair') {
+                                    console.log(chalk.bgYellow('Não existe esse elemento na lista'))
+                                } else if (entrada == 'sair') {
+                                    console.log(chalk.yellow('Finalizando ...'))
+                                } else if (lista.indexOf(`"${entrada}"`) != -1) {
+                                    lista.splice(lista.indexOf(`"${entrada}"`), 1)
+                                    console.log(chalk.bgGreenBright(`A propriedade ${entrada} foi removida da lista`))
+                                }
+                            }
+                            let result = lista.sort().join("\n")
+                            console.log(chalk.bgCyanBright(result))
+                            CriarArquivo(lista)
+                        }
+                    })
+            } catch {
+                console.log(chalk.bgRedBright(`Arquivo não encontrado! Verifique o que foi digitado e tente Novamente`))
+                EditarPropriedades()
+            }
+        })
+}
+function Puxalista(listaedit) {
+    const accountJSON = fs.readFileSync(`plista/${listaedit}.json`, {
+        encoding: 'utf-8',
+        flag: 'r',//quebra linha
     })
     return JSON.parse(accountJSON)
 }
-function Visualizardados(){
+function Visualizardados() {
     inquirer
-    .prompt([
-        {
-            name:'listaatual',
-            message:'Nome do arquivo que possui os itens?'
+        .prompt([
+            {
+                name: 'listaatual',
+                message: 'Nome do arquivo.json que possui os itens?'
 
-        },
-    ])
-    .then((answer) => {
-        const listaedit =answer['listaatual']
-        const listaed = Puxalista(listaedit)
-        
-        let listavelha = listaed.list
-        let lista = []
-        for (let cot=0; cot<listavelha.length;cot++){
-            lista.push(`"${listavelha[cot]}"`)
-        }
-        let result= lista.sort().join("\n")
-        console.log(result)
-    })
+            },
+        ])
+        .then((answer) => {
+            try{
+                const listaedit = answer['listaatual']
+                const listaed = Puxalista(listaedit)
+
+                let listavelha = listaed.list
+                let lista = []
+                for (let cot = 0; cot < listavelha.length; cot++) {
+                    lista.push(`"${listavelha[cot]}"`)
+                }
+                let result = lista.sort().join("\n")
+                console.log(chalk.bgBlue.yellowBright("Lista com Propriedades CSS:"))
+                console.log(chalk.bgCyanBright(result))
+                ProgMain()
+            }catch{
+                console.log(chalk.bgRedBright(`Arquivo não encontrado! Verifique o que foi digitado e tente Novamente`))
+                Visualizardados()
+            }
+        })
 }
